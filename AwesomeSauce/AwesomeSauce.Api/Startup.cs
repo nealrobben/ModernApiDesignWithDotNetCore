ac3c625b58d34590dbcd3a6a474e2a22b1122c55
Neal Robben <neal.robben@cegeka.com>
2019-07-15 13:34:52 +0200

    Added endpoints for Foo and Bar

--- AwesomeSauce/AwesomeSauce.Api/Startup.cs
+++ AwesomeSauce/AwesomeSauce.Api/Startup.cs
@@ -25,6 +25,22 @@ namespace AwesomeSauce.Api
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Map("/foo",
+                config =>
+                    config.Use(async (context, next) =>
+                        await context.Response.WriteAsync("Welcome to /foo")
+                    )
+            );
+            app.MapWhen(
+                context =>
+                    context.Request.Method == "POST" &&
+                    context.Request.Path == "/bar",
+                config =>
+                    config.Use(async (context, next) =>
+                        await context.Response.WriteAsync("Welcome to POST /bar")
+                    )
+            );
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
